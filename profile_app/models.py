150e46789fad72e800e1b578272be0eef6f4da33
asiuri <104019846+asiuri@users.noreply.github.com>
2022-08-04 15:52:50 +0200

    add

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ConfirmCode(models.Model):
+    code=models.CharField(max_length=8)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
